2150e963b75357c9751c94f9afd22a18c945a82a
Thomas Benz <tbenz@iis.ee.ethz.ch>
2023-06-29 10:25:09 +0200

    treewide: Setup repository
    * Extract AXI-RT from AXI
    
    * Update to multi-manager multi-subordinate testbench

---
+++ include/axi-rt/assign.svh
@@ -0,0 +1,61 @@
+// Copyright (c) 2014-2018 ETH Zurich, University of Bologna
+//
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// Authors:
+// - Thomas Benz <tbenz@iis.ee.ethz.ch>
+
+// Macros to assign AXI Interfaces and Structs
+
+`ifndef AXI_RT_ASSIGN_SVH_
+`define AXI_RT_ASSIGN_SVH_
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Macros instantiating highlighter
+// `AXI_HIGHLIGHT(__name, __aw_t, __w_t, __b_t, __ar_t, __r_t, __req, __resp)
+`define AXI_HIGHLIGHT(__name, __aw_t, __w_t, __b_t, __ar_t, __r_t, __req, __resp) \
+  signal_highlighter #(                                                           \
+    .T ( __aw_t )                                                                 \
+  ) i_signal_highlighter_``__name``_aw (                                          \
+    .ready_i ( __resp.aw_ready ),                                                 \
+    .valid_i ( __req.aw_valid  ),                                                 \
+    .data_i  ( __req.aw        )                                                  \
+  );                                                                              \
+  signal_highlighter #(                                                           \
+    .T ( __w_t )                                                                  \
+  ) i_signal_highlighter_``__name``_w (                                           \
+    .ready_i ( __resp.w_ready ),                                                  \
+    .valid_i ( __req.w_valid  ),                                                  \
+    .data_i  ( __req.w        )                                                   \
+  );                                                                              \
+  signal_highlighter #(                                                           \
+    .T ( __b_t )                                                                  \
+  ) i_signal_highlighter_``__name``_b (                                           \
+    .ready_i ( __req.b_ready  ),                                                  \
+    .valid_i ( __resp.b_valid ),                                                  \
+    .data_i  ( __resp.b       )                                                   \
+  );                                                                              \
+  signal_highlighter #(                                                           \
+    .T ( __ar_t )                                                                 \
+  ) i_signal_highlighter_``__name``_ar (                                          \
+    .ready_i ( __resp.ar_ready ),                                                 \
+    .valid_i ( __req.ar_valid  ),                                                 \
+    .data_i  ( __req.ar        )                                                  \
+  );                                                                              \
+  signal_highlighter #(                                                           \
+    .T ( __r_t )                                                                  \
+  ) i_signal_highlighter_``__name``_r (                                           \
+    .ready_i ( __req.r_ready  ),                                                  \
+    .valid_i ( __resp.r_valid ),                                                  \
+    .data_i  ( __resp.r       )                                                   \
+  );
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+`endif
---
+++ scripts/start.tcl
@@ -0,0 +1,3 @@
+vsim -voptargs=+acc -t 1ps tb_axi_rt_unit
+
+log -r /*
